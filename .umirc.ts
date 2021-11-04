206b1a47341d52e94e3adfa008fcfeab8ab2d8cd
xuezhenjiang <zhenjiang.xue@zatech.com>
2021-11-04 10:33:48 +0800

    update

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  base: '/siteExample/',
+  publicPath: '/siteExample/',
+  title: 'Frontend Blog',
   nodeModulesTransform: {
     type: 'none',
   },
